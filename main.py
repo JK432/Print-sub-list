b9d25cf57ed2038e98aaf803fce1378b9b0ce97b
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-22 05:19:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Write a Python program to print the sub-list of a given list according to the user specified range
+x=int(input())
+y=int(input())
+list=['python','c++','java','javascript','html','css','dart']
+print(list[x:y])
+
